2a73bbd580f0d1a6ce67518514a8579890fdbe93
Vishesh Dubey <visheshdubey2016@gmail.com>
2025-02-17 00:49:16 +0530

    Update content-about.tsx

--- data/content-about.tsx
+++ data/content-about.tsx
@@ -24,7 +24,7 @@ export const profileData: ProfileData = {
   },
   name: "visheshdubey.",
   title: "Full-Stack Engineer",
-  description: `👋 I’m a web developer, a freelancer specializing in building modern, scalable web applications. I work with React, Next.js, SvelteKit, and backend frameworks like Hono.js and Express.js.`,
+  description: `👋 I’m a full-stack developer, a freelancer specializing in building modern, scalable web applications. I work with React, Next.js, SvelteKit, and backend frameworks like Hono.js and Express.js.`,
   socialLinks: [
     { href: "http://#", icon: Github, className: "w-5 stroke-muted-foreground" },
     { href: "http://#", icon: Linkedin, className: "w-5 stroke-muted-foreground" },
