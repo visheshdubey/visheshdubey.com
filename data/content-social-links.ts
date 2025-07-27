72ec18ebfde1ced54160bbe687def7b38bd0e966
visheshdubey <visheshdubey2016@gmail.com>
2025-07-27 16:41:20 +0530

    deleted all files

--- data/content-social-links.ts
+++
@@ -1,6 +0,0 @@
-export const socialLinks = {
-    github: "https://github.com/visheshdubey",
-    linkedin: "https://www.linkedin.com/in/visheshdubey/",
-    twitter: "https://x.com/visheshdubey_",
-    email: "mailto:visheshdubey.work@gmail.com",
-};
