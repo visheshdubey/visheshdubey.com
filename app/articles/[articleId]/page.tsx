4c65d1921e2109c07f539f7b7e60588ec068d677
visheshdubey <visheshdubey2016@gmail.com>
2025-07-27 17:54:54 +0530

    refactor(config): update Next.js configuration and dependencies

--- app/articles/[articleId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const ArticleDetailPage = (props: Props) => {
-    return <div>ArticleDetailPage</div>;
-};
-
-export default ArticleDetailPage;
--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,14 @@ import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+    // Configure `pageExtensions` to include markdown and MDX files
+    pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+    // Optionally, add any other Next.js config below
 };
 
 const withMDX = createMDX({
-    extension: /\.mdx?$/,
+    // Add markdown plugins here, as desired
 });
 
+// Merge MDX config with Next.js config
 export default withMDX(nextConfig);
