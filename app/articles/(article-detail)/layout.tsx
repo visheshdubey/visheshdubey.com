8501c408a61f6e16a96fdbe8df8b403fe896b932
visheshdubey <visheshdubey2016@gmail.com>
2026-03-23 08:54:00 +0530

    refactor(layout): adjust layout dimensions and improve styling for article detail

--- app/articles/(article-detail)/layout.tsx
+++ app/articles/(article-detail)/layout.tsx
@@ -33,16 +33,16 @@ export default function LayoutBlogPost({ children }: { children: React.ReactNode
         <>
             <div className="pointer-events-none fixed left-0 top-0 z-10 h-12 w-full bg-gray-100 to-transparent backdrop-blur-xl [-webkit-mask-image:linear-gradient(to_bottom,black,transparent)] dark:bg-zinc-950" />
             <ScrollProgress
-                className="fixed top-0 z-20 h-0.5 bg-gray-300 dark:bg-zinc-600"
+                className="fixed top-0 z-20 h-0.5 bg-red-300 dark:bg-zinc-600"
                 // springOptions={{
                 //     bounce: 0,
                 // }}
             />
 
-            <div className="absolute right-4 top-24">
+            <div className="absolute left-4 top-24">
                 <CopyButton />
             </div>
-            <main className="prose prose-gray mt-24 pb-20 prose-h4:prose-base dark:prose-invert prose-h1:text-xl prose-h1:font-medium prose-h2:mt-12 prose-h2:scroll-m-20 prose-h2:text-lg prose-h2:font-medium prose-h3:text-base prose-h3:font-medium prose-h4:font-medium prose-h5:text-base prose-h5:font-medium prose-h6:text-base prose-h6:font-medium prose-strong:font-medium">
+            <main className="prose max-w-full prose-gray mt-24 pb-20 prose-h4:prose-base dark:prose-invert prose-h1:text-xl prose-h1:font-medium prose-h2:mt-12 prose-h2:scroll-m-20 prose-h2:text-lg prose-h2:font-medium prose-h3:text-base prose-h3:font-medium prose-h4:font-medium prose-h5:text-base prose-h5:font-medium prose-h6:text-base prose-h6:font-medium prose-strong:font-medium">
                 {children}
             </main>
         </>
--- app/layout.tsx
+++ app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
             <body className={`${geistSans.variable} ${geistMono.variable} bg-white tracking-tight antialiased dark:bg-zinc-950`}>
                 <ThemeProvider enableSystem={true} attribute="class" storageKey="theme" defaultTheme="system">
                     <div className="flex min-h-screen w-full flex-col font-[family-name:var(--font-inter-tight)]">
-                        <div className="relative mx-auto w-full max-w-screen-sm flex-1 px-4 pt-20">
+                        <div className="relative mx-auto w-full max-w-screen-md flex-1 px-4 pt-20">
                             <Header />
                             {children}
                             <Footer />
